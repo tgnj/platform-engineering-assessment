a85a439ee2da671781e731f8413b908e4a34158f
Terence Goh Nai Jiu <terence.goh@gmail.com>
2024-04-24 13:28:12 -0700

    fix: visits spec

--- spec/consumers/visits_consumer_spec.rb
+++ spec/consumers/visits_consumer_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 RSpec.describe VisitsConsumer do
   subject(:consumer) { karafka.consumer_for(:visits) }
 
--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 RSpec.describe Visit do
   subject(:visit) do
     described_class.create!(
