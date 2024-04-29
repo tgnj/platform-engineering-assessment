9f7b12abde1a482bcc7d28e7ef1bd4556b6546e0
Terence Goh Nai Jiu <terence.goh@gmail.com>
2024-04-29 16:47:27 -0700

    fix: add rspec for simple attendance consumer

--- app/consumers/attendance_consumer.rb
+++ app/consumers/attendance_consumer.rb
@@ -1,20 +1,38 @@
 # frozen_string_literal: true
 
 class AttendanceConsumer < ApplicationConsumer
-  # Consumes the messages by inserting all of them in one go into the DB
+  REQUIRED_KEYS = %w[entity_identifier site_identifier event].freeze
+  OPTIONAL_KEYS = %w[sign_in_time sign_out_time].freeze
+
+  def verify_payload(payload)
+    return false unless REQUIRED_KEYS.all? { |key| payload.key?(key) }
+    return false unless OPTIONAL_KEYS.any? { |key| payload.key?(key) }
+    true
+  end
+
+  def event_sign_in(payload)
+    Attendance.create(payload.slice('entity_identifier', 'site_identifier', 'sign_in_time'))
+  end
+
+  def event_sign_out(payload)
+    attendance = Attendance.where(entity_identifier: payload['entity_identifier'],
+                                  site_identifier: payload['site_identifier'],
+                                  sign_out_time: nil).first
+
+    attendance && attendance.update(sign_out_time: payload['sign_out_time'])
+  end
+
+  # Consumes the messages by inserting all of them in one go into the DB. We will create this
   def consume
-    messages.payloads.each do |message_payload|
-      case message_payload['event']
-      when 'sign_in'
-        Attendance.create!(message_payload.except('event'))
+    puts ">>>>>>>>> consume"
+    messages.each do |message|
+      payload = message.payload
 
-      when 'sign_out'
-        Attendance \
-          .where(entity_identifier: message_payload['entity_identifier'], site_identifier: message_payload['site_identifier']) \
-          .last \
-          .update(sign_out_time: message_payload['sign_out_time'])
-      end
+      next unless verify_payload payload
 
+      send :"event_#{payload['event']}", payload
+
+      mark_as_consumed(message)
     end
   end
 end
--- spec/consumers/attendance_consumer_spec.rb
+++ spec/consumers/attendance_consumer_spec.rb
@@ -4,22 +4,43 @@ require 'spec_helper'
 
 RSpec.describe AttendanceConsumer do
   subject(:consumer) { karafka.consumer_for(:attendance) }
-  let(:visitor_id) { SecureRandom.uuid }
-  let(:attendances) do
-    Array.new(2) do
+
+  let(:samples) do
+    Array.new(4) do
       {
-        id: SecureRandom.uuid,
-        visited_at: Time.zone.now,
-        visitor_id: visitor_id,
-        page_path: '/'
+        entity_identifier: "PCO:CHILD:#{SecureRandom.uuid}",
+        site_identifier: "PCO:SITE:#{SecureRandom.uuid}"
       }
     end
   end
 
-  # Publish two visits of one user
-  before { attendances.each { |attendance| karafka.produce(attendance.to_json) } }
+  describe "event sign_in" do
+    let(:sign_ins) do
+      samples.map { |t| t.merge(sign_in_time: Time.zone.now, event: 'sign_in') }
+    end
 
-  it 'expects to save the attendance' do
-    expect { consumer.consume }.to change(Attendance, :count).by(attendances.count)
+    it 'works' do
+      sign_ins.each { |sign_in| karafka.produce(sign_in.to_json) }
+      expect { consumer.consume }.to change(Attendance, :count).by(sign_ins.count)
+    end
   end
+
+  describe "event sign_out" do
+    let(:sign_outs) do
+      samples.map { |t| t.merge(sign_out_time: Time.zone.now, event: 'sign_out') }
+    end
+
+    before do
+      sign_ins = samples.map { |t| t.merge(sign_in_time: Time.zone.now) }
+      sign_ins.each { |sign_in| Attendance.create(sign_in) }
+    end
+
+    it 'expects to update the attendance' do
+      Attendance.destroy_all
+
+      sign_outs.each { |sign_out| karafka.produce(sign_out.to_json) }
+      expect { consumer.consume }.not_to change(Attendance, :count)
+    end
+  end
+
 end
